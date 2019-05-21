7856ebe1c4262d6baef3f8bfd5e52916ecc9085e
scc85 <sarahchristinec85@gmail.com>
2019-05-20 21:41:04 -0700

    redirects file

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 class Projects extends Component {
     render() {
         return (
-            <div className="container">
+            <div className="container">x
 
                 {/* Project One */}
                 <div className="row">
